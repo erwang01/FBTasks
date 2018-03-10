5632e84f1055748d0c107e98e0e6aaf23ab5a263
sarah feng <sarah@onlab.us>
2018-03-10 15:36:22 -0800

    bug fix

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def send_message(recipient_id, message):
         }
     }
     header = {"Content-Type: application/json"}
-    fb_response = requests.post(endpointURL, data=json.dumps(payload), headers=headers)
+    fb_response = requests.post(endpointURL, data=json.dumps(payload), headers=header)
     return fb_response
 
 app.config['DEBUG'] = True
