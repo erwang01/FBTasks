1d4ed44652e4df56368be129789332373edbfeb4
sarah feng <sarah@onlab.us>
2018-03-10 23:24:49 -0800

    optional https

--- app.py
+++ app.py
@@ -49,8 +49,8 @@ def verify_fb_token(token_sent):
     return 'Nothing to see here.'
 
 url_regex = re.compile(
-        r'^(?:http|ftp)s?://' # http:// or https://
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'^(?:http|ftp)s?://?' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' # domain
         r'localhost|' #localhost...
         r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
         r'(?::\d+)?' # optional port
