e79afab88af4be8b32d06954bf976ff28669469b
Eric Wang <erwang01@gmail.com>
2018-03-10 21:39:03 -0800

    Update Page Access Token

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os 
 
 os.environ['VERIFY_TOKEN'] = "SDJGIENKFJ66665"
-os.environ['PAGE_ACCESS_TOKEN'] = "EAAH2jdKoLmwBAJhIW0jnGkzqIuqayoLJIjTyLrtg9R7muaTD2yqjkksFkY57MDx9IXdcnsK2qHub9ypHDgOYWyAbNehtGgZBgDZB5xAMp9jrZAL0XZCmA71NhvgpRPlJvEeLKBHZCtUDm7tVsWdINXYCgOO4KFIsYFA6cPwxOKAZDZD"
+os.environ['PAGE_ACCESS_TOKEN'] = "EAAcTknAhZB8gBAPHwKkIZC4DXqbP2qSC1m0g3xGcfEStEcKh8a8epaxibnIyLuuNlvgRERAMhpp4lMzZBQ6egQ5lakwutKH5JrF4AOj9V3hwnZBkKH8SA5FPb9r0n8ZCSJj5SogLf6hePBShQYQsQuEaUvcmUzyNj8ayGRhflrgZDZD"
 os.environ['DATABASE_URL'] = "postgres://olsjbtriowyfxz:2284b5a03d2dc47cab1c488c983c552af1d72b125f19202f15fc89995122d2a9@ec2-54-221-220-59.compute-1.amazonaws.com:5432/d3o61mo7stnajv"
