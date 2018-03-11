e3d8f2c6843ba86f132ccb5df247b5e1e0599982
sarah feng <sarah@onlab.us>
2018-03-11 01:06:09 -0800

    bug fixes

--- app.py
+++ app.py
@@ -82,11 +82,11 @@ def handle_message(sender_id, message):
                 replaced = o
                 if o.scheme!='http' and o.scheme!='https':
                    replaced = o._replace(scheme='http')
-                   print('This url was altered to ' + replaced.get_url())
+                   print('This url was altered to ' + replaced.geturl())
                 if not o.netloc: 
                     pass
                 try:
-                    requests.get(replaced.get_url(), timeout = .75)
+                    requests.get(replaced.geturl(), timeout = .75)
                     urls.append(url_string)
                 except:
                     pass
